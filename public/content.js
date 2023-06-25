4cfff6a278d36e03aadb6574c1da238f5ee44778
alexanderlin <alexraptordev@gmail.com>
2023-06-25 11:49:49 -0400

    clean up comments

--- public/content.js
+++ public/content.js
@@ -13,40 +13,12 @@ function getAllNodes() {
   }
   return collectionOfNodes;
 }
-//function searchKeyWords(keywordArray) {
-//  const nodes = getAllNodes();
-//  const keyWords = [];
-//  for (const node of nodes) {
-//    if (node.nodeValue) {
-//      const text = node.nodeValue.split(" ");
-//      for (let word of text) {
-//        word = word.toLowerCase();
-//        if (keywordArray.includes(word)) {
-//          // const nodeAndKeyword = {node,word};
-//          keyWords.push(word);
-//        }
-//      }
-//    }
-//  }
-//  return keyWords;
-//}
-//function keywordsToUrls(keyWordData) {
-//  const keywords = searchKeyWords(keyWordData);
-//  const baseUrl = document.documentElement.baseURI;
-//  const textFragment = "#:~:text=";
-//  const Urls = [];
-//  for (const keyword of keywords) {
-//    Urls.push(baseUrl + textFragment + keyword + "\n");
-//  }
-//  return Urls;
-//
-//}
 
 function searchKeyWords(keywordArray) {
   const nodes = getAllNodes();
   const keyWords = {};
-  for(const key of keywordArray){
-    keyWords[key] = {count: 0,links: []};
+  for (const key of keywordArray) {
+    keyWords[key] = { count: 0, links: [] };
   }
   for (const node of nodes) {
     if (node.nodeValue) {
@@ -63,23 +35,11 @@ function searchKeyWords(keywordArray) {
   }
   return keyWords;
 }
-function keyWordToUrl(node){
+function keyWordToUrl(node) {
   const baseUrl = node.baseURI;
   const textFragment = "#:~:text=";
   return baseUrl + textFragment + node.nodeValue;
 }
-//function keywordsToUrls(keyWordData) {
-//  const nodes = searchKeyWords(keyWordData);
-//  const baseUrl = document.documentElement.baseURI;
-//  const textFragment = "#:~:text=";
-//  const Urls = [];
-//  for (const node of nodes) {
-//    Urls.push(baseUrl + textFragment + node.nodeValue + "\n");
-//  }
-//  return Urls;
-//
-//}
-// Function called when a new message is received
 const messagesFromReactAppListener = (message, sender, response) => {
 
   console.log('[content.js]. Message received', message, sender);
